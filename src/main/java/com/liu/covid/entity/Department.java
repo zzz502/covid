e45510e4008e2c6edac5a29e9d06b18dd5be64a8
zzz <1648254202@qq.com>
2022-11-12 19:52:03 +0800

    111

--- src/main/java/com/liu/covid/entity/Department.java
+++ src/main/java/com/liu/covid/entity/Department.java
@@ -10,4 +10,45 @@ public class Department {
     private String name;
     private String charge;
 
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", charge='" + charge + '\'' +
+                '}';
+    }
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name, String charge) {
+        this.id = id;
+        this.name = name;
+        this.charge = charge;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharge() {
+        return charge;
+    }
+
+    public void setCharge(String charge) {
+        this.charge = charge;
+    }
 }
